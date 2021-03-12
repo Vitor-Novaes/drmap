aa3eab69efc58254181addd5ba4e935201cbf608
Gleidson Ferreira <gleidson.fgs@gmail.com>
2021-03-12 20:01:06 -0300

    wip: Fix service

--- lib/http_service.dart
+++ lib/http_service.dart
@@ -4,21 +4,35 @@ import 'package:drm/models/road_map.dart';
 import 'package:http/http.dart' as http;
 
 class HttpService {
-  final String uri =
-      "https://8bp38ux9fi.execute-api.us-east-1.amazonaws.com/development";
+  final String uri = "8bp38ux9fi.execute-api.us-east-1.amazonaws.com";
 
   Future<List<RoadMap>> getAllRoadMaps() async {
-    final response = await http.get(Uri.https(uri, '/roadmap'));
-    print(response);
-    if (response.statusCode == 200) {
-      List<dynamic> body = jsonDecode(response.body);
+    try {
+      final response = await http.get(Uri.https(uri, 'development/roadmap'));
+      print(response);
+      print('print json decode');
+      print(jsonDecode(response.body));
+      if (response.statusCode == 200) {
+        var test = json.decode(response.body);
+        var map = Map<String, dynamic>.from(test);
+        print(map['body']);
 
-      List<RoadMap> roadmaps =
-          body.map((dynamic item) => RoadMap.fromJson(item)).toList();
+        print(map['body'].map((dynamic item) => item));
+        // List<RoadMap> roadmaps =
+        var list = map['body'].map((dynamic item) => RoadMap.fromJson(item));
+        print("lista: $list");
+        print(list.cast<List<RoadMap>>());
 
-      return roadmaps;
-    } else {
-      throw 'Cant get roadmaps';
+        return list.cast<List<RoadMap>>();
+      } else {
+        print('Cant get roadmaps error');
+        throw 'Cant get roadmaps';
+      }
+    } catch (e) {
+      print("finished with exceptions $e");
+      return [RoadMap(id: 1, title: 'Vazio', hex: '#fff')];
+    } finally {
+      print("finished with exceptions");
     }
   }
 }
